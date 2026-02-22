f5be8683209fa960f26a1b62be95cd7ec4592a5d
Vid <vid@example.com>
2026-02-22 09:36:00 +0530

    ui: finalize PiecePage.jsx implementation

--- src/PiecePage.jsx
+++ src/PiecePage.jsx
@@ -5,5 +5,11 @@ function PiecePage({ piece }) {
     <div className="piece-page">
       <h1 className="text-5xl text-center">{piece} Piece</h1>
       <p className="description text-xl text-center">
-        The {piece} is one of the most important
-// WIP
+        The {piece} is one of the most important pieces in the game of chess...
+        {/* Add detailed descriptions here */}
+      </p>
+    </div>
+  );
+}
+
+export default PiecePage;
